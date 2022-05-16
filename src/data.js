baf1832fcce09a4121b49b4e5bd1e15d2a4bfdf5
vasyasuchevan <penguin12.tp@gmail.com>
2022-05-16 17:25:12 +0300

    routing

--- src/data.js
+++ src/data.js
@@ -1,5 +1,4 @@
-export const sliderItems = [
-	{
+export const sliderItems = [{
 		id: 1,
 		img: "https://i.imgur.com/LG4wFeC.png",
 		title: "SUMMER SALE",
@@ -24,29 +23,27 @@ export const sliderItems = [
 	}
 ];
 
-export const categories = [
-	{
+export const categories = [{
 		id: 1,
-		img:
-			"https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+		img: "https://images.pexels.com/photos/428340/pexels-photo-428340.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
 		title: "SHIRT STYLE!",
+		cat: "women"
 	},
 	{
 		id: 2,
-		img:
-			"https://images.pexels.com/photos/792725/pexels-photo-792725.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+		img: "https://images.pexels.com/photos/792725/pexels-photo-792725.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
 		title: "LOUNGEWEAR LOVE",
+		cat: "coat"
 	},
 	{
 		id: 3,
-		img:
-			"https://images.pexels.com/photos/6856000/pexels-photo-6856000.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+		img: "https://images.pexels.com/photos/6856000/pexels-photo-6856000.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
 		title: "LIGHT JACKETS",
+		cat: "jens"
 	},
 ];
 
-export const popularProducts = [
-	{
+export const popularProducts = [{
 		id: 1,
 		img: "https://optimusgang.com/wp-content/uploads/2022/03/%D0%9E%D0%93-%D1%85-%D0%9F-%D1%83%D0%BA%D1%80-%D0%BD%D0%BE%D0%B2%D1%8B%D0%B5-%D1%86%D0%B2%D0%B5%D1%82%D0%B0_0001s_0001_%D0%BF%D0%B5%D1%80%D0%B5%D0%B4.jpg"
 	},
